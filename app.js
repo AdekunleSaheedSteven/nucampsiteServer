54b80f35998ee9670291e81dfb141bf9ba573d5a
Saheed Steven Adekunle <assmokson@gmail.com>
2023-09-29 23:57:53 -0500

    Cookies

--- app.js
+++ app.js
@@ -38,56 +38,70 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+//providing cookie-parse with a secret key as an argument. The secret key can be any string. It doesn't has to be anything meaningful, it is just a cryptographic key that can be used by cookieParse to encrypt information and sign the cookie that sent from server to the client
+app.use(cookieParser("12345-67890-54321"));
 
 //adding Basic Authentication
 function auth(req, res, next) {
-  console.log(req.headers);
+  //the signedCookies property of request object is provided by cookieParse.It will automatically parse signed cookies from the request. If the cookie is not properly signed then it will return a value of false. The additional property called 'user' is the property we ourselve add to the signed cookie. So if the incoming request doesn't include signedCookie.user property or the signed cookie value then it will parse as false so we know the client has not be authenticate then we challenge the client to aunthenticate.
+  if (!req.signedCookies.user) {
+    //grab authorization from request header
+    const authHeader = req.headers.authorization;
 
-  //grab authorization from request header
-  const authHeader = req.headers.authorization;
+    //server not getting any authorization credential from client
+    if (!authHeader) {
+      const err = new Error("You are not authenticated!");
 
-  //server not getting any authorization credential from client
-  if (!authHeader) {
-    const err = new Error("You are not authenticated!");
+      //this let the client know the server is requesting authentication and the authentication is basic.
+      res.setHeader("WWW-Authenticate", "Basic");
 
-    //this let the client know the server is requesting authentication and the authentication is basic.
-    res.setHeader("WWW-Authenticate", "Basic");
+      // this is standard error code if credential is not provided.
+      err.status = 401;
 
-    // this is standard error code if credential is not provided.
-    err.status = 401;
+      //this will send the error message back to the client.
+      return next(err);
+    }
 
-    //this will send the error message back to the client.
-    return next(err);
-  }
+    //if the authorization is availbale inside the header sent by the client then all below will happen. Means there is authorization header then our code will skip to below codes.
+    //the authorization header will be parse and then valid the username and password.
+    //the authorization header will contain the word BASIC then space follow by the username and password in Based-64 encoding string. Once the code is decoded it will show the username and password separate by colon.
+    //in this case we need to take out the username and password out of the HEADER string and put them into a new array.
+    //Username will hold index 1 and password will hold index 2 inside the array
+    //put the array inside const auth then use the buffer global class in Node. Since Buffer is global in Node means we do not need to REQUIRE it.
+    //the Buffer has the static method FROM to decode the username and password Buffer.from()
+    //the code inside Buffer.from() will pick the Based-64 encoding username and password and extract the username and password to be able to read and then put them into auth array as first and second items.
+    const auth = Buffer.from(authHeader.split(" ")[1], "base64")
+      .toString()
+      .split(":");
 
-  //if the authorization is availbale inside the header sent by the client then all below will happen. Means there is authorization header then our code will skip to below codes.
-  //the authorization header will be parse and then valid the username and password.
-  //the authorization header will contain the word BASIC then space follow by the username and password in Based-64 encoding string. Once the code is decoded it will show the username and password separate by colon.
-  //in this case we need to take out the username and password out of the HEADER string and put them into a new array.
-  //Username will hold index 1 and password will hold index 2 inside the array
-  //put the array inside const auth then use the buffer global class in Node. Since Buffer is global in Node means we do not need to REQUIRE it.
-  //the Buffer has the static method from to decode the username and password Buffer.from()
-  //the code inside Buffer.from() will pick the Based-64 encoding username and password and extract the username and password to be able to read and then put them into auth array as first and second items.
-  const auth = Buffer.from(authHeader.split(" ")[1], "base64")
-    .toString()
-    .split(":");
+    //grab the username and password out of the array.
+    const user = auth[0];
+    const pass = auth[1];
 
-  //grab the username and password out of the array.
-  const user = auth[0];
-  const pass = auth[1];
+    //this is a Basic validation
+    if (user === "admin" && pass === "password") {
+      //the res.cookie() method is part of Express response API. We use it to create a new cookie and pass it a name we want to use for the cookie user and this name will be used to set up a property of user on the signed cookie object.
+      //the second value will be a value to store a name property and we give it a string of 'admin'
+      //the third argument is optional, it is an object that contains configuration value. By setting '{signed: true}' we let the Express know to use the key from cookieParse to create a signed cookie. This res.cookie() method handles creation of the cookie and set it up in the server response to the client.
+      res.cookie("user", "admin", { signed: true });
+      //if successful then access is granted
+      return next(); // authorized
 
-  //this is a Basic validation
-  if (user === "admin" && pass === "password") {
-    //if successful then access is granted
-    return next(); // authorized
-
-    //if fail then will send error msg to client
+      //if fail then will send error msg to client
+    } else {
+      const err = new Error("You are not authenticated!");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
   } else {
-    const err = new Error("You are not authenticated!");
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    return next(err);
+    if (res.signedCookies.user === "admin") {
+      return next();
+    } else {
+      const err = new Error("You are not authenticated!");
+      err.status = 401;
+      return next(err);
+    }
   }
 }
 
